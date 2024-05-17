634e0e31caff336404a34c79941a795035f33101
profsam97 <proftoby4real@hotmail.com>
2024-05-17 18:23:57 +0100

    refactored code

--- src/dtos/task/update-task.dto.ts
+++ src/dtos/task/update-task.dto.ts
@@ -1,9 +1,17 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
+import { IsAtLeastOneDefined } from 'src/decorators/custom-validator-decorators';
 
-export class UpdateTask {
+export class UpdateTaskDto {
+  @IsOptional()
   @IsString()
-  name: string;
+  name?: string;
 
+  @IsOptional()
   @IsString()
-  description: string;
+  description?: string;
+
+  @IsAtLeastOneDefined(['name', 'description'], {
+    message: 'At least one of name or description must be defined',
+  })
+  update: string; 
 }
