9d453415d433f063f791d4a161404f5337eaae04
profsam97 <proftoby4real@hotmail.com>
2024-05-17 20:03:09 +0100

    updated task interface

--- src/interfaces/task.interface.ts
+++ src/interfaces/task.interface.ts
@@ -1,11 +1,11 @@
 export interface ITask {
-    owner: number,
-    name: string,
-    description: string,
-    createdAt: Date,
-    updatedAt: sd
+  owner: number;
+  name: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: sdsdsd;
 }
 
 export interface IUpdateTaskParams {
-    id : string
+  id: string;
 }
