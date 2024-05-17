89f595df7407a7c2a63ce535463a98760798f963
profsam97 <proftoby4real@hotmail.com>
2024-05-17 18:27:16 +0100

    updated app module to include auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guard/auth-gaurd';
 
 @Module({
-  imports: [],
+  imports: [TaskModule, UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{
+    provide : APP_GUARD,
+    useClass: AuthGuard
+  },
+  AppService
+]
 })
 export class AppModule {}
