c3d76f1b3038b8027fd7bea7a7963a615cee0ca4
profsam97 <proftoby4real@hotmail.com>
2024-06-10 22:14:53 +0100

    added git workflows

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,11 @@
-
-import {Body, Controller, HttpCode, HttpStatus, Injectable, Post} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Injectable,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from 'src/dtos/auth/signup.dto';
 import { IMessage } from 'src/interfaces/message.interface';
@@ -14,35 +20,31 @@ import { RequestPasswordResetDto } from 'src/dtos/auth/password-reset.dto';
 @Injectable()
 @Controller('api/v1/auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-
-  ) {}
+  constructor(private readonly authService: AuthService) {}
   @Public()
   @Post('/sign-up')
   public async SignUp(@Body() signUpDto: SignupDto): Promise<IMessage> {
-    
     try {
-        return await this.authService.signup(signUpDto);
+      console.log('sdsd');
+      return await this.authService.signup(signUpDto);
     } catch (error) {
-      errorHandler(error)
+      console.log(error);
+      errorHandler(error);
     }
   }
 
   @Public()
   @Post('/sign-in')
   @HttpCode(HttpStatus.OK)
-  public async SignIn(@Body() signInDto : SigninDto) : Promise<IAuthUser> {
-    
-        return await this.authService.signin(signInDto)
-    
+  public async SignIn(@Body() signInDto: SigninDto): Promise<IAuthUser> {
+    return await this.authService.signin(signInDto);
   }
 
   @Post('forgot-password')
   @HttpCode(HttpStatus.OK)
   async sendOtp(@Body() sendOtpDto: SendOtpDto): Promise<string> {
-        const otp : string = await this.authService.sendOtp(sendOtpDto);
-        return otp;
+    const otp: string = await this.authService.sendOtp(sendOtpDto);
+    return otp;
   }
 
   @Post('verify-otp')
@@ -50,21 +52,22 @@ export class AuthController {
   async verifyOtp(@Body() verifyOtpDto: VerifyOtpDto): Promise<string> {
     try {
       await this.authService.verifyOtp(verifyOtpDto);
-      return 'Verified successfully, please reset'
+      return 'Verified successfully, please reset';
     } catch (error) {
-      errorHandler(error)
+      errorHandler(error);
     }
   }
 
   @Post('reset-password')
   @HttpCode(HttpStatus.OK)
-  async resetPassword(@Body() resetPasswordDto: RequestPasswordResetDto): Promise<string> {
+  async resetPassword(
+    @Body() resetPasswordDto: RequestPasswordResetDto,
+  ): Promise<string> {
     try {
       await this.authService.resetPassword(resetPasswordDto);
-      return 'Password reset was successful'
+      return 'Password reset was successful';
     } catch (error) {
-      errorHandler(error)
+      errorHandler(error);
     }
   }
- 
 }
